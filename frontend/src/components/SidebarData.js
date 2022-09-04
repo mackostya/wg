9778e6247eebbfa1056be953558315db023ae3da
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2022-09-04 14:16:12 +0200

    frontend sidebar

---
+++ frontend/src/components/SidebarData.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+
+export const SidebarData = [
+    {
+        title: "home",
+        icon: <HomeIcon/>, 
+        link: "/home"
+    },
+    {
+        title: "todos",
+        icon: <CheckBoxIcon/>, 
+        link: "/todos"
+    },
+    {
+        title: "calendar",
+        icon: <CalendarMonthIcon/>, 
+        link: "/calendar"
+    }
+]
